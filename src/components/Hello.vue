cc5ca5a83ebb763d457f278ad83b3ec36b749327
Benson <472756921@qq.com>
2017-09-21 15:13:11 +0800

    ''

---
+++ src/components/Hello.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="layout">
+    <Row type="flex" style="height: 100%">
+      <Col span="3" class="layout-menu-left">
+      <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+        <div class="layout-logo-left">华扬咨询</div>
+        <MenuItem name="1"><i class="iconfont icon-clock"></i>预约管理</MenuItem>
+        <MenuItem name="2"><i class="iconfont icon-businesscard"></i>用户管理</MenuItem>
+        <MenuItem name="3"><i class="iconfont icon-barrage"></i>服务单</MenuItem>
+        <MenuItem name="4"><i class="iconfont icon-addressbook"></i>员工管理</MenuItem>
+        <MenuItem name="5"><i class="iconfont icon-document"></i>投诉管理</MenuItem>
+        <MenuItem name="6"><i class="iconfont icon-createtask"></i>库存管理</MenuItem>
+        <MenuItem name="7"><i class="iconfont icon-manage"></i>项目管理</MenuItem>
+        <MenuItem name="8"><i class="iconfont icon-dynamic"></i>经营报表</MenuItem>
+      </Menu>
+      </Col>
+      <Col span="21">
+      <div class="layout-header">
+        <span class="title">美容院门店系统</span>
+        <div class="adminInfo">
+          <Avatar icon="person" size="large" style="vertical-align: middle"/>
+          <span class="user"> &nbsp;&nbsp;刘德华 <Icon type="arrow-down-b"></Icon>
+          </span>
+          <span> &nbsp;&nbsp;|&nbsp;&nbsp; </span>
+          <span class="user">退出&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        </div>
+      </div>
+      <div class="layout-content">
+        <div class="layout-content-main">
+          <router-view></router-view>
+        </div>
+      </div>
+      <div class="layout-copy">
+        2017-2018 &copy; BENSON
+      </div>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .ivu-menu-item-active:not(.ivu-menu-submenu){
+    background-color: #F7EFFE;
+    color: #66368C!important;
+    font-weight: 600;
+    border-right: 2px solid #66368C!important;
+  }
+  .user:hover{
+    cursor: pointer;
+    font-weight: 600;
+    color: #66368C;
+  }
+  .title{
+    font-weight: 600;
+    font-size: 20px;
+  }
+  .adminInfo{
+    float: right;
+  }
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    height: 100%;
+  }
+  .layout-content{
+    min-height: 300px;
+    margin: 10px;
+    overflow: hidden;
+    background: #fff;
+    border-radius: 4px;
+    height: 85%;
+  }
+  .layout-content-main{
+    padding: 10px;
+  }
+  .layout-copy{
+    text-align: center;
+    padding: 10px 0 20px;
+    color: #9ea7b4;
+  }
+  .layout-menu-left{
+    background: #fff;
+  }
+  .layout-header{
+    background: #fff;
+    padding: 22px 20px;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+  }
+  .layout-logo-left{
+    border-radius: 3px;
+    font-size: 20px;
+    padding: 22px 0;
+    color: #fff;
+    background-color: #66368C;
+    text-align: center;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueResource from 'vue-resource';
+import axios from 'axios';
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import './my-theme/index.less';
+
+
+Vue.config.productionTip = false
+Vue.use(VueResource);
+Vue.use(iView);
+Vue.prototype.$ajax = axios;
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
