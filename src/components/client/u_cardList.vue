eee272cadb20145edb82ede722edb632f141b18d
Benson <472756921@qq.com>
2017-10-29 16:34:19 +0800

    chang

--- src/components/client/u_cardList.vue
+++ src/components/client/u_cardList.vue
@@ -9,11 +9,13 @@
     name: 'u_cardList',
     data(){
       return {
-        question: [
-          {key: 'date', title: '添加日期'},
-          {key: 'question', title: '问题描述'},
-          {key: 'resti', title: '决绝方案',  render:(h, p)=>{if(p.row.resti === 1){return '基础方案'}else if(p.row.resti === 2 ){return '推荐方案'} else if(p.row.resti === 3 ) {return '最优方案'}else if(p.row.resti === 0 ) {return '未指定'}}},
-          {key: 'action', title: '操作', render: (h, params) => {
+        cards: [
+          {key: 'date', title: '购卡日期'},
+          {key: 'type', title: '卡类'},
+          {key: 'times', title: '剩余次数'},
+          {key: 'status', title: '状态'},
+          {key: 'other', title: '备注'},
+          {key: 'action', title: '操作',render: (h, params) => {
             return h('div', [
               h('Button', {
                 props: {
@@ -28,7 +30,7 @@
                     this.datile(params.index)
                   }
                 }
-              }, '解决方案'),
+              }, '退卡'),
               h('Button', {
                 props: {
                   type: 'primary',
@@ -46,11 +48,8 @@
             ]);
           }},
         ],
-        questionData: [
-          {date: '2012-12-12', question: '皮肤暗沉', resti: 1},
-          {date: '2012-12-12', question: '眼部皮肤松弛', resti: 2},
-          {date: '2012-12-12', question: '眼袋', resti: 3},
-          {date: '2012-12-12', question: '黄褐斑', resti: 0},
+        cardsData: [
+          {date: '2012-12-12', type: '产品卡', times: '22', status: 1, other: '无'},
         ],
       }
     },
--- src/components/client/u_datile.vue
+++ src/components/client/u_datile.vue
@@ -22,48 +22,6 @@
     name: 'u_datile',
     data(){
       return {
-        cards: [
-          {key: 'date', title: '购卡日期'},
-          {key: 'type', title: '卡类'},
-          {key: 'times', title: '剩余次数'},
-          {key: 'status', title: '状态'},
-          {key: 'other', title: '备注'},
-          {key: 'action', title: '操作',render: (h, params) => {
-            return h('div', [
-              h('Button', {
-                props: {
-                  type: 'primary',
-                  size: 'small'
-                },
-                style: {
-                  marginRight: '5px'
-                },
-                on: {
-                  click: () => {
-                    this.datile(params.index)
-                  }
-                }
-              }, '退卡'),
-              h('Button', {
-                props: {
-                  type: 'primary',
-                  size: 'small'
-                },
-                style: {
-                  marginRight: '5px'
-                },
-                on: {
-                  click: () => {
-                    this.datile(params.index)
-                  }
-                }
-              }, '操作'),
-            ]);
-          }},
-        ],
-        cardsData: [
-          {date: '2012-12-12', type: '产品卡', times: '22', status: 1, other: '无'},
-        ],
       };
     },
     methods: {
