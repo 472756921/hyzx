2d3773d58ab936567b17e9b94ba58052f4250d02
Benson <472756921@qq.com>
2017-10-29 16:31:35 +0800

    chang

--- src/components/client/u_datile.vue
+++ src/components/client/u_datile.vue
@@ -8,11 +8,15 @@
       <TabPane label="服务记录" name="name1">
         <Table :columns="service" :data="serviceData"></Table>
       </TabPane>
-      <TabPane label="消费记录" name="name2">
+      <TabPane label="消费记录(现金)" name="name2">
         <Table :columns="shop" :data="shopData" :row-class-name="rowClassName"></Table>
       </TabPane>
-      <TabPane label="问题分析" name="name3">标签三的内容</TabPane>
-      <TabPane label="现有卡情况" name="name3">标签三的内容</TabPane>
+      <TabPane label="问题分析" name="name3">
+        <Table :columns="question" :data="questionData"></Table>
+      </TabPane>
+      <TabPane label="现有卡情况" name="name4">
+        <Table :columns="cards" :data="cardsData"></Table>
+      </TabPane>
     </Tabs>
   </div>
 </template>
@@ -55,8 +59,8 @@
         ],
         shop: [
           {key: 'date', title: '日期'},
-          {key: 'status', title: '行为', render:(h, p)=>{if(p.row.status === 1){return '充值'}else if(p.row.status === 2 ){return '购买产品'} else {return '退款'}}},
           {key: 'price', title: '金额'},
+          {key: 'status', title: '行为', render:(h, p)=>{if(p.row.status === 1){return '充值'}else if(p.row.status === 2 ){return '购买产品'} else if(p.row.status === 0 ) {return '退款'}}},
           {key: 'p_name', title: '产品'},
         ],
         shopData: [
@@ -64,11 +68,105 @@
           {date: '2012-12-12', status: 1, price: 12000, p_name: '储值卡'},
           {date: '2012-12-12', status: 0, price: 12000, p_name: '储值卡'},
         ],
+        question: [
+          {key: 'date', title: '添加日期'},
+          {key: 'question', title: '问题描述'},
+          {key: 'resti', title: '决绝方案',  render:(h, p)=>{if(p.row.resti === 1){return '基础方案'}else if(p.row.resti === 2 ){return '推荐方案'} else if(p.row.resti === 3 ) {return '最优方案'}else if(p.row.resti === 0 ) {return '未指定'}}},
+          {key: 'action', title: '操作', render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.datile(params.index)
+                  }
+                }
+              }, '解决方案'),
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.datile(params.index)
+                  }
+                }
+              }, '操作'),
+            ]);
+          }},
+        ],
+        questionData: [
+          {date: '2012-12-12', question: '皮肤暗沉', resti: 1},
+          {date: '2012-12-12', question: '眼部皮肤松弛', resti: 2},
+          {date: '2012-12-12', question: '眼袋', resti: 3},
+          {date: '2012-12-12', question: '黄褐斑', resti: 0},
+        ],
+        cards: [
+          {key: 'date', title: '购卡日期'},
+          {key: 'type', title: '卡类'},
+          {key: 'times', title: '剩余次数'},
+          {key: 'status', title: '状态'},
+          {key: 'other', title: '备注'},
+          {key: 'action', title: '操作',render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.datile(params.index)
+                  }
+                }
+              }, '退卡'),
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.datile(params.index)
+                  }
+                }
+              }, '操作'),
+            ]);
+          }},
+        ],
+        cardsData: [
+          {date: '2012-12-12', type: '产品卡', times: '22', status: 1, other: '无'},
+        ],
       };
+    },
+    methods: {
+      rowClassName(r) {
+        if(r.status == 0){
+          return 'redBack';
+        }
+      }
     },
   };
 </script>
 
-<style scoped>
-
+<style>
+  .ivu-table .redBack td{
+    background-color: #fff4f3;
+  }
 </style>
---
+++ src/components/client/u_serviceList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <Table :columns="service" :data="serviceData"></Table>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'u_serviceList',
+    data(){
+      return {
+        service: [
+          {key: 'date', title: '日期'},
+          {key: 'number', title: '服务单号'},
+          {key: 'e_name', title: '技师'},
+          {key: 'room', title: '房间'},
+          {
+            key: 'zd', title: '是否指定', render: (h, p)=> {
+            if (p.zd == 1) {
+              return '指定'
+            } else {
+              return '非指定'
+            }
+          }
+          },
+          {
+            key: 'pay_way', title: '支付方式', render: (h, p)=> {
+            if (p.pay_way == 1) {
+              return '卡扣'
+            } else {
+              return '现金'
+            }
+          }
+          },
+          {
+            title: '操作',
+            key: 'action',
+            render: (h, params) => {
+              return h('div', [h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.datile(params.index)
+                  }
+                }
+              }, '详细'),
+              ]);
+            }
+          },
+        ],
+        serviceData: [
+          {date: '2012-12-12', e_name: '张小泉', zd: 1, pay_way: 1, room: 203, number: 123123},
+        ],
+      }
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
