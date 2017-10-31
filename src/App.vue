82291596e1cbf73bb9d9f3bfdde1f5a3275c4691
Benson <472756921@qq.com>
2017-10-31 18:25:06 +0800

    chang

--- src/App.vue
+++ src/App.vue
@@ -11,31 +11,31 @@ export default {
 </script>
 
 <style>
-  a:link {color: #66368C}		/* 未访问的链接 */
-  a:visited {color: #66368C}	/* 已访问的链接 */
-  a:hover {color: #66368C}	/* 鼠标移动到链接上 */
-  a:active {color: #66368C}
+  a:link {color: #66368C!important;}		/* 未访问的链接 */
+  a:visited {color: #66368C!important;}	/* 已访问的链接 */
+  a:hover {color: #66368C!important;}	/* 鼠标移动到链接上 */
+  a:active {color: #66368C!important;}
   .prC{
-    background-color: #66368C;
+    background-color: #66368C!important;;
   }
   .ivu-input-group-append{
-    background-color: #66368C;
-    color: #fff;
-    border: none;
+    background-color: #66368C!important;;
+    color: #fff!important;;
+    border: none!important;;
   }
   .hy_btn{
-    background-color: #66368C;
-    color: #fff;
+    background-color: #66368C!important;
+    color: #fff!important;;
   }
   .hy_btn:hover {
-    border-color: #66368C;
-    color: #66368C;
+    border-color: #66368C!important;
+    color: #66368C!important;;
   }
   .option{
-    padding: 10px 10px 16px;
-    border-bottom: 10px solid #F5F7F9;
+    padding: 10px 10px 16px!important;;
+    border-bottom: 10px solid #F5F7F9!important;;
   }
   #app{
-    min-width: 1100px;
+    min-width: 1100px!important;;
   }
 </style>
